bc7b860378dced7f30d3b550ddfdd3fd64b9f489
Eder Canada <canada.eder@gmail.com>
2019-11-20 15:56:53 -0800

    register form complete and active

---
+++ night_in/src/Register/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+
+class Register extends Component {
+  state = {
+    username: '',
+    password: '',
+    email: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    const registerResponse = await fetch(`${process.env.REACT_APP_API_URL}/user/register`,{
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify(this.state),
+      headers: {
+        'Content-Type' : 'application/json'
+       
+      }
+      
+    })
+
+    const parsedResponse = await registerResponse.json();
+
+    console.log(parsedResponse)
+
+    if(parsedResponse.status.code === "Success"){
+      console.log('we did it')
+      this.props.doUpdateCurrentUser(parsedResponse.data)
+      this.props.history.push('/')
+    }
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <small> Username</small>
+        <input type='text' name="username" onChange={this.handleChange} />
+        <small> Password</small>
+        <input type='password' name="password" onChange={this.handleChange} />
+        <small> Email</small>
+        <input type='text' name="email" onChange={this.handleChange} />
+        <button type="Submit" color="green">Register</button>
+      </form>
+    )
+  }
+}
+
+export default Register
