3c018f020075136a2911af978912944068a24301
Tim Malstead <timmmalstead@gmail.com>
2019-11-21 13:39:38 -0800

    creates navbar

--- night_in/src/Navbar/index.js
+++ night_in/src/Navbar/index.js
@@ -1,1 +1,11 @@
-//this is the navbar
+import React from 'react';
+
+function Navbar()  {
+    return (
+        <div>
+            <p>this is the navbar</p>
+        </div>
+    )
+}
+
+export default Navbar
